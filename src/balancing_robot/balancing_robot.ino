b0c8f696e0f0fdd8b632e9532dc9b47dab2f41bd
Roman V. Prikhodchenko <chujoii@gmail.com>
2012-05-03 09:46:53 +0400

    2012-05-03.2

--- src/balancing_robot/balancing_robot.ino
+++ src/balancing_robot/balancing_robot.ino
@@ -1,12 +1,34 @@
-int ma = 9;
-int mb = 10
-void setup() {                
-  pinMode(13, OUTPUT);     
+int led_pin = 13;      // select the pin for the LED
+
+int motor1_pin_e = 9;
+int motor1_pin_a = 10;
+int motor1_pin_b = 11;
+
+
+void setup() {
+  // declare the ledPin as an OUTPUT:
+  pinMode(led_pin, OUTPUT);
+
+  pinMode(motor1_pin_e, OUTPUT);
+  pinMode(motor1_pin_a, OUTPUT);
+  pinMode(motor1_pin_b, OUTPUT);
+
 }
 
+
+int sensor
+
+
 void loop() {
-  digitalWrite(13, HIGH);   // set the LED on
-  delay(1000);              // wait for a second
-  digitalWrite(13, LOW);    // set the LED off
-  delay(1000);              // wait for a second
+
+  digitalWrite(led_pin, HIGH);
+  digitalWrite(motor1_pin_a, HIGH);
+  digitalWrite(motor1_pin_b, HIGH);
+  analogWrite(motor1_pin_e, 256);
+  delay(2000);
+  digitalWrite(led_pin, LOW);
+  delay(2000);
+  
 }
+
+
