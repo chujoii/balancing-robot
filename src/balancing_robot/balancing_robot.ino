f1b389a75d50636263cf27d63a564d574d31c841
Roman V. Prikhodchenko <chujoii@gmail.com>
2015-12-23 10:23:32 +0300

    statistic

--- src/balancing_robot/balancing_robot.ino
+++ src/balancing_robot/balancing_robot.ino
@@ -1,3 +1,13 @@
+/*
+  balancing_robot.ino - Program for balancing robot
+  Created by Roman V. Prikhodchenko, 2012.04(apr)
+  Released into the GPLv3.
+ */
+
+#define PROPORTIONAL 0
+#define INTEGRAL 1
+#define DERIVATIVE 2
+
 #include <Wire.h> //Include the Wire library
 #include <MMA_7455.h> //Include the MMA_7455 library
 
@@ -18,14 +28,26 @@ const int max_pwm = 255;
 
 float global_zero_angle = 0.0;
 
-float k_p = 1.0;
+float global_coefficient[]          = {1.0, 1.0, 1.0};
+float global_previous_coefficient[] = {1.0, 1.0, 1.0};
+float global_coefficient_step[] = {1.0, 1.0, 1.0};
+float global_func = 0.0;          // for search extremum of function
+float global_previous_func = 0.0; // for search extremum of function
 
-float k_i = 0.0;
-double sum_of_angle = 0.0;
+double global_sum_of_angle = 0.0;
+float global_previous_angle = 0.0;
+unsigned long global_previous_time = 0;
 
-float k_d = 0.0;
-float previous_angle = 0.0;
-unsigned long previous_time = 0;
+
+const unsigned int learn_duration_time_ms = 10000;
+unsigned int learn_time_ms = 0;
+float learn_max_angle = 0.0;
+float learn_min_angle = 0.0;
+unsigned int learn_start_turn_time_us = 0;
+unsigned int learn_end_turn_time_us = 0;
+unsigned int learn_max_turn_time_us = 0;
+boolean learn_between_start_end_time = false;
+int learn_coeff_num = 0; // 0 Proportional; 1 Integral; 2 Derivative algorithm
 
 boolean fast_init = false;
 
@@ -72,29 +94,59 @@ void motor_control(int speed)
 
 
 
-float PID(float angle)
+float PID(float previous_angle, unsigned int previous_time, float current_angle, unsigned int current_time)
 {
 	// Proportional Integral Derivative algorithm
 
 	float control = 0.0;
 
         // Proportional coefficient
-	control = k_p * angle;
+	control = global_coefficient[PROPORTIONAL] * current_angle;
 	
         // Integral coefficient
-	sum_of_angle += angle;
-	control += k_i * sum_of_angle;
+	global_sum_of_angle += current_angle;
+	control += global_coefficient[INTEGRAL] * global_sum_of_angle;
 	
         // Derivative coefficient
-	unsigned long current_time = micros();
-	control += k_d * (angle - previous_angle)/(current_time - previous_time);
-	previous_angle = angle; // save angle
-	previous_time = current_time; // save time
+	control += global_coefficient[DERIVATIVE] * (current_angle - previous_angle)/(current_time - previous_time);
 	
 	return control;
 }
 
+void statistic(float previous_angle, unsigned int previous_time, float current_angle, unsigned int current_time)
+{
+	if (previous_angle<0.0 && current_angle>=0.0 && learn_between_start_end_time==false) {
+		learn_start_turn_time_us = micros();
+		learn_between_start_end_time = true;
+	}
+	if (previous_angle>0.0 && current_angle<=0.0 && learn_between_start_end_time==true) {
+		learn_end_turn_time_us   = micros();
+		learn_between_start_end_time = false;
+		unsigned int dtse = learn_end_turn_time_us - learn_start_turn_time_us;
+		if (learn_max_turn_time_us < dtse) {learn_max_turn_time_us = dtse;}
+	}
+	if (learn_max_angle < current_angle) {learn_max_angle = current_angle;}
+	if (learn_min_angle > current_angle) {learn_min_angle = current_angle;}
+}
 
+void new_learn()
+{
+	learn_max_angle = 0.0;
+	learn_min_angle = 0.0;
+	learn_start_turn_time_us = 0;
+	learn_end_turn_time_us = 0;
+	learn_max_turn_time_us = 0;
+	learn_between_start_end_time = false;
+	
+	learn_coeff_num++;
+	if (learn_coeff_num>3){learn_coeff_num = 0;}
+}
+
+void learn(int coeff_num)
+{
+	// Gauss–Seidel method -OR- method of successive displacement -OR- iterative method of coordinate descent
+	float result;
+}
 
 void setup()
 {
@@ -144,16 +196,27 @@ void setup()
 
 void loop()
 {
-	float angle;
+	float current_angle = read_inclinometer();
+	unsigned long current_time = micros();
+	
 	float motor_speed;
 
-	angle = read_inclinometer();
-	motor_speed = PID(angle);
+	if (learn_time_ms<millis()){
+		learn_time_ms = millis() + learn_duration_time_ms;
+		learn(learn_coeff_num);
+		new_learn();
+	}
+	
+	motor_speed = PID(global_previous_angle, global_previous_time, current_angle, current_time);
 	motor_control(motor_speed);
+	statistic(global_previous_angle, global_previous_time, current_angle, current_time);
+		
+	global_previous_angle = current_angle; // save angle
+	global_previous_time = current_time; // save time
 
 	if (debug){
 		Serial.print("\tangle=");
-		Serial.print(angle);
+		Serial.print(current_angle);
 		Serial.print("\tmotor=");
 		Serial.print(motor_speed);
 		
